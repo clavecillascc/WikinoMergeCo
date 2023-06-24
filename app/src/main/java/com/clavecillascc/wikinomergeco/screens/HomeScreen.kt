4c98b7dc6bb20458df5311c8a44ddb4d5f61bbb7
Alexis Roman <romanalexisnicole06@gmail.com>
2023-06-24 20:14:58 +0800

    Added Shadow

--- app/src/main/java/com/clavecillascc/wikinomergeco/screens/HomeScreen.kt
+++ app/src/main/java/com/clavecillascc/wikinomergeco/screens/HomeScreen.kt
@@ -2,9 +2,11 @@ package com.clavecillascc.wikinomergeco.screens
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -13,11 +15,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.OffsetEffect
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.unit.dp
 import com.clavecillascc.wikinomergeco.ui.theme.appWhiteYellow
+import com.clavecillascc.wikinomergeco.ui.theme.appYellow
 
 @Composable
 fun HomeScreen() {
@@ -25,6 +32,7 @@ fun HomeScreen() {
     Column(modifier = Modifier
         .verticalScroll(rememberScrollState())
         ) {
+        Spacer(modifier = Modifier.size(15.dp))
         WordOfTheDay()
         Feedbox2()
         WordOfTheDay()
@@ -40,12 +48,20 @@ fun WordOfTheDay(
     ) {
     Column(
         modifier = Modifier
-            .padding(15.dp)
+            .padding(10.dp)
+            .shadow(
+                shape = RoundedCornerShape(10.dp),
+                elevation = 5.dp,
+            )
             .clip(RoundedCornerShape(10.dp))
             .background(color)
             .padding(horizontal = 15.dp, vertical = 20.dp)
+
             .fillMaxWidth()
             .height(200.dp)
+
+
+
     ) {
         //Header
         Text(
@@ -82,6 +98,10 @@ fun Feedbox2(
     Column(
         modifier = Modifier
             .padding(15.dp)
+            .shadow(
+                shape = RoundedCornerShape(10.dp),
+                elevation = 5.dp,
+            )
             .clip(RoundedCornerShape(10.dp))
             .background(color)
             .padding(horizontal = 15.dp, vertical = 20.dp)
