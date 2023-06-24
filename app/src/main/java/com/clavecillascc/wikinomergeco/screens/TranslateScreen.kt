b90010e2902bd9cdd8b392ff8cf42d67cc4df1ec
Alexis Roman <romanalexisnicole06@gmail.com>
2023-06-24 20:53:31 +0800

    Added initial library buttons

--- app/src/main/java/com/clavecillascc/wikinomergeco/screens/TranslateScreen.kt
+++ app/src/main/java/com/clavecillascc/wikinomergeco/screens/TranslateScreen.kt
@@ -1,10 +1,24 @@
 package com.clavecillascc.wikinomergeco.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.clavecillascc.wikinomergeco.ui.theme.appWhiteYellow
 
 @Composable
 fun TranslateScreen() {
     //TODO
     Text(text = "Translate")
 }
+
