fd4c51874ad0b88a5fc54719f9b893d96a4234eb
Angelo Miguel N. de Padua <angelosenpai3@gmail.com>
2023-07-05 02:06:45 +0800

    word of the day content with randomizer

--- app/src/main/java/com/clavecillascc/wikinomergeco/screens/HomeScreen.kt
+++ app/src/main/java/com/clavecillascc/wikinomergeco/screens/HomeScreen.kt
@@ -13,6 +13,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
@@ -25,6 +28,10 @@ import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.unit.dp
 import com.clavecillascc.wikinomergeco.ui.theme.appWhiteYellow
 import com.clavecillascc.wikinomergeco.ui.theme.appYellow
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 @Composable
 fun HomeScreen() {
@@ -35,16 +42,37 @@ fun HomeScreen() {
         Spacer(modifier = Modifier.size(15.dp))
         WordOfTheDay()
         Feedbox2()
-        WordOfTheDay()
-        WordOfTheDay()
     }
 }
 
 @Composable
 fun WordOfTheDay(
     color: Color = appWhiteYellow,
+) {
+    val word = remember { mutableStateOf("") }
 
-    ) {
+    val storage = FirebaseStorage.getInstance()
+    val storageRef = storage.reference
+    val ONE_MEGABYTE: Long = 1024 * 1024
+
+    LaunchedEffect(Unit) {
+        // Retrieve a list of all files in the desired folder
+        val files = withContext(Dispatchers.IO) {
+            storageRef.child("Words/").listAll().await().items
+        }
+
+        // Choose a random file from the list
+        val randomFile = files.random()
+
+        // Download the content of the random file
+        val bytes = withContext(Dispatchers.IO) {
+            randomFile.getBytes(ONE_MEGABYTE).await()
+        }
+
+        val text = String(bytes)
+        word.value = text
+    }
+
     Column(
         modifier = Modifier
             .padding(10.dp)
@@ -55,39 +83,28 @@ fun WordOfTheDay(
             .clip(RoundedCornerShape(10.dp))
             .background(color)
             .padding(horizontal = 15.dp, vertical = 20.dp)
-
             .fillMaxWidth()
             .height(200.dp)
-
-
-
     ) {
-        //Header
+        // Header
         Text(
             text = "Sample",
             style = MaterialTheme.typography.headlineMedium
-
         )
-        //Word of the Day
+        // Word of the Day
         Text(
-            text = "",
+            text = word.value,
             style = MaterialTheme.typography.titleMedium
-
         )
-        //Other terms
+        // Other terms
         Text(
             text = ""
-
         )
-        //In sentence
+        // In sentence
         Text(
             text = ""
-
         )
-
-
     }
-
 }
 
 @Composable
