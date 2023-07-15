7f7ab9f2de6f5655a71641a175b7490601c93a6e
Christian C. Clavecillas <clavecillascc@gmail.com>
2023-07-15 09:12:14 +0800

    Changed Login

--- app/src/main/java/com/clavecillascc/wikinomergeco/MainActivity.kt
+++ app/src/main/java/com/clavecillascc/wikinomergeco/MainActivity.kt
@@ -2,20 +2,39 @@ package com.clavecillascc.wikinomergeco
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.clavecillascc.wikinomergeco.loginAndSignUp.LoginAndSignUpViewModel
-import com.clavecillascc.wikinomergeco.mainScreen.MainScreenViewModel
-import com.clavecillascc.wikinomergeco.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.clavecillascc.wikinomergeco.mainScreen.MainScreen
+import com.clavecillascc.wikinomergeco.signin.GoogleAuthUiClient
+import com.clavecillascc.wikinomergeco.signin.SignInScreen
+import com.clavecillascc.wikinomergeco.signin.SignInViewModel
 import com.clavecillascc.wikinomergeco.ui.theme.WikinoMergeCoTheme
+import com.google.android.gms.auth.api.identity.Identity
 import com.google.firebase.FirebaseApp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val googleAuthUiClient by lazy {
+        GoogleAuthUiClient(
+            context = applicationContext,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+    }
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +44,78 @@ class MainActivity : ComponentActivity() {
 
         installSplashScreen()
         setContent {
-            val loginAndSignUpViewModel = viewModel(modelClass = LoginAndSignUpViewModel::class.java)
-            val mainScreenViewModel = viewModel(modelClass = MainScreenViewModel::class.java)
             WikinoMergeCoTheme {
-                Navigation(
-                    loginAndSignUpViewModel = loginAndSignUpViewModel,
-                    mainScreenViewModel = mainScreenViewModel
-                )
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "sign_in") {
+                        composable("sign_in") {
+                            val viewModel = viewModel<SignInViewModel>()
+                            val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            LaunchedEffect(key1 = Unit) {
+                                if(googleAuthUiClient.getSignedInUser() != null) {
+                                    navController.navigate("main_screen")
+                                }
+                            }
+
+                            val launcher = rememberLauncherForActivityResult(
+                                contract = ActivityResultContracts.StartIntentSenderForResult(),
+                                onResult = { result ->
+                                    if(result.resultCode == RESULT_OK) {
+                                        lifecycleScope.launch {
+                                            val signInResult = googleAuthUiClient.signInWithIntent(
+                                                intent = result.data ?: return@launch
+                                            )
+                                            viewModel.onSignInResult(signInResult)
+                                        }
+                                    }
+                                }
+                            )
+
+                            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                                if(state.isSignInSuccessful) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Sign in successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+
+                                    navController.navigate("main_screen")
+                                    viewModel.resetState()
+                                }
+                            }
+
+                            SignInScreen(
+                                state = state,
+                                onSignInClick = {
+                                    lifecycleScope.launch {
+                                        val signInIntentSender = googleAuthUiClient.signIn()
+                                        launcher.launch(
+                                            IntentSenderRequest.Builder(
+                                                signInIntentSender ?: return@launch
+                                            ).build()
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                        composable("main_screen") {
+                            MainScreen(
+                                userData = googleAuthUiClient.getSignedInUser(),
+                                onSignOut = {
+                                    lifecycleScope.launch {
+                                        googleAuthUiClient.signOut()
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "Signed out",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                        navController.popBackStack()
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
             }
         }
     }
-}
