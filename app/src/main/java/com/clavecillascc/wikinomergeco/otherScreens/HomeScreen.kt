124bd535e5d854bf506e9e68c918ef682cb3093e
Christian C. Clavecillas <clavecillascc@gmail.com>
2023-07-30 20:43:54 +0800

    FAQ UI

--- app/src/main/java/com/clavecillascc/wikinomergeco/otherScreens/HomeScreen.kt
+++ app/src/main/java/com/clavecillascc/wikinomergeco/otherScreens/HomeScreen.kt
@@ -1,14 +1,20 @@
 package com.clavecillascc.wikinomergeco.otherScreens
 import android.util.Log
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -17,9 +23,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -192,133 +200,101 @@ fun FAQ(
             .clip(RoundedCornerShape(10.dp))
             .background(color)
             .padding(horizontal = 15.dp, vertical = 15.dp)
-            .fillMaxWidth()
-            .height(200.dp),
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-
         Column {
-            //Header
             Text(
                 text = "FAQ",
-                style = MaterialTheme.typography.headlineMedium,
-
-                )
-            //Word of the Day
-            Text(
-                text = "   "+"• "+"How do I navigate to different screens in the app?",
-                style = MaterialTheme.typography.labelMedium
-
-            )
-            //Other terms
-            Text(
-                text = "   "+"• "+"How do I add a new word?",
-                style = MaterialTheme.typography.labelMedium
-
-            )
-            //In sentence
-            Text(
-                text = "   "+"• "+"How do I view my recently added contributions?",
-                style = MaterialTheme.typography.labelMedium
-
-            )
-        }
-        }
-    }
-
-
-@Composable
-fun HomeForum ( color: Color = appWhiteYellow){
-    Column(
-        modifier = Modifier
-            .padding(start = 20.dp, top = 10.dp, end = 20.dp, bottom = 80.dp)
-            .shadow(
-                shape = RoundedCornerShape(10.dp),
-                elevation = 5.dp,
-            )
-            .clip(RoundedCornerShape(10.dp))
-            .background(color)
-            .padding(horizontal = 15.dp, vertical = 15.dp)
-            .fillMaxWidth()
-            .height(200.dp),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-
-        Column {
-            //Header
-            Text(
-                text = "Forum",
-                style = MaterialTheme.typography.headlineMedium,
-
-                )
-            //Word of the Day
-            Text(
-                text = "",
-                style = MaterialTheme.typography.titleMedium
-
-            )
-            //Other terms
-            Text(
-                text = ""
-
-            )
-            //In sentence
-            Text(
-                text = ""
-
-            )
-
-        }
-        //See more clickable
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "See more",
-                modifier = Modifier.align(alignment = Alignment.End),
-                style = MaterialTheme.typography.displaySmall,
-
-                )
-
+                style = MaterialTheme.typography.headlineMedium)
+            ExpandableBox1()
+            Spacer(modifier = Modifier.size(15.dp))
+            ExpandableBox2()
         }
     }
 }
 
 @Composable
-fun Translation(color: Color = appWhiteYellow){
-    Column {
-        //Line 2 -Translated Word
-        Row {
-            Text(
-                text = "     " + "Naglamis",
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                text = "(Ilocano)",
-                style = MaterialTheme.typography.titleMedium
-            )
+fun ExpandableBox1(color: Color = appWhiteYellow) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val height by animateDpAsState(if (isExpanded) 180.dp else 50.dp)
+    val width by animateDpAsState(if (isExpanded) 300.dp else 300.dp)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .animateContentSize()
+            .background(color)
+            .clickable {
+                isExpanded = !isExpanded
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(start = 2.dp, top = 2.dp, end = 2.dp, bottom = 2.dp)
+                .shadow(
+                    shape = RoundedCornerShape(5.dp),
+                    elevation = 5.dp,
+                )
+                .clip(RoundedCornerShape(5.dp))
+                .background(color)
+                .padding(horizontal = 5.dp, vertical = 5.dp)
+                .height(height)
+                .width(width)
+                .background(appWhiteYellow)
+        ) {
+            Column {
+                Text(
+                    text = "   " + "• " + "How do I navigate to different screens in the app?",
+                    style = MaterialTheme.typography.labelMedium
+                )
+                Text(
+                    text = "   " + "• " + "You can navigate to different screens using the bottom navigation bar at the bottom of the screen. Each icon represents a different screen. Simply tap on the icon to go to the corresponding screen.",
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
         }
+    }
+}
 
-        //Line 3 - Other terms
-        Text(
-            text = "     "  + "• " + "ang lamig, malamig",
-            style = MaterialTheme.typography.titleSmall
-
-        )
-        Spacer(modifier = Modifier.size(10.dp))
-        //Line 4 - Sentence in same language
-        Text(
-            text ="       "  + "Naglamiis ti angin!",
-            style = MaterialTheme.typography.bodySmall
-        )
-        //Line 5 - Sentence in chosen language
-        Text(
-            text = "          "  + "Ang lamig ng hangin!",
-            style = MaterialTheme.typography.headlineSmall
-        )
-        //Line 6 - Sentence in english language
-
-        Text(
-            text = "          "  + "The wind is so cold!",
-            style = MaterialTheme.typography.headlineSmall
-        )
+@Composable
+fun ExpandableBox2(color: Color = appWhiteYellow) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val height by animateDpAsState(if (isExpanded) 210.dp else 25.dp)
+    val width by animateDpAsState(if (isExpanded) 300.dp else 300.dp)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .animateContentSize()
+            .background(color)
+            .clickable {
+                isExpanded = !isExpanded
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(start = 2.dp, top = 2.dp, end = 2.dp, bottom = 2.dp)
+                .shadow(
+                    shape = RoundedCornerShape(5.dp),
+                    elevation = 5.dp,
+                )
+                .clip(RoundedCornerShape(5.dp))
+                .background(color)
+                .padding(horizontal = 5.dp, vertical = 5.dp)
+                .height(height)
+                .width(width)
+                .background(appWhiteYellow)
+        ) {
+            Column {
+                Text(
+                    text = "   " + "• " + "How do I add a new word?",
+                    style = MaterialTheme.typography.labelMedium
+                )
+                Text(
+                    text = "   " + "• " + "To add a new collaborator, navigate to the 'Collaborator' screen using the drawer menu found in the top bar. Click on the floating action button (the plus icon) in the bottom right corner. Fill in the required information for the new word and click 'Upload' to add them.",
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+        }
     }
 }
